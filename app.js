170ec76dfd5e7fbb465c7de0a6351fd44b212821
David Whittaker <davidwhittaker@macbook-pro.lan>
2020-07-01 10:42:06 +0100

    update to design + got rid of logo hover effect on small devices

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const logo = document.querySelector(".logo");
 
-logo.addEventListener("mouseenter", ()=> {
-    logo.setAttribute("src", "./dave_1.1.svg");
-    logo.classList.add("logo-hover");
-});
+function hoverEffect() {
+console.log(window.innerWidth);
 
-logo.addEventListener("mouseleave", ()=> {
-    logo.setAttribute("src", "./dave_1.0.svg");
-    logo.classList.remove("logo-hover");
-})
+  if (window.innerWidth > 1000) {
+    logo.addEventListener("mouseenter", () => {
+      logo.setAttribute("src", "./dave_1.1.svg");
+      logo.classList.add("logo-hover");
+    });
 
-logo.addEventListener("click", topFunction)
+    logo.addEventListener("mouseleave", () => {
+      logo.setAttribute("src", "./dave_1.0.svg");
+      logo.classList.remove("logo-hover");
+    });
+  }
+}
+
+
+hoverEffect();
+window.addEventListener("resize", hoverEffect);
+
+logo.addEventListener("click", topFunction);
 
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
-  
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
