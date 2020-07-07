ac1e0ba3b20086a291085da98754a680f6fb48f8
David Whittaker <davidwhittaker@macbook-pro.lan>
2020-07-07 12:27:17 +0100

    upate to skills section

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
-const logo = document.querySelector(".logo");
-
-function hoverEffect() {
-console.log(window.innerWidth);
-
-  if (window.innerWidth > 1000) {
-    logo.addEventListener("mouseenter", () => {
-      logo.setAttribute("src", "./dave_1.1.svg");
-      logo.classList.add("logo-hover");
-    });
-
-    logo.addEventListener("mouseleave", () => {
-      logo.setAttribute("src", "./dave_1.0.svg");
-      logo.classList.remove("logo-hover");
-    });
+document.addEventListener('scroll', function(e) {
+   if(window.pageYOffset >= 330){
+       document.querySelector(".logo").style.opacity = 0;
+   }
+   if(window.pageYOffset < 330){
+    document.querySelector(".logo").style.opacity = 1;
+}
+  
   }
-}
-
-
-hoverEffect();
-window.addEventListener("resize", hoverEffect);
-
-logo.addEventListener("click", topFunction);
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+);
